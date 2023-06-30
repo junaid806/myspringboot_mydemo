f8fc0b201bf2420a2d6a8992fed8519093e75fe7
junaid806 <52387021+junaid806@users.noreply.github.com>
2023-07-01 00:53:00 +0530

    Add files via upload

---
+++ springboot-demo/src/test/java/com/stara/agile/assignment/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stara.agile.assignment.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
